1374f11b2d843b076ad3afddbf59324d98607e73
Thanuharan <Thanuharanvilvanathan05@gmail.com>
2025-03-21 00:15:11 +0530

    Add files via upload

---
+++ Day1/Day1.js
@@ -0,0 +1,73 @@
+console.log("Hello World");
+
+let a=9;
+let b=3;
+
+
+let add=a+b;
+let sub=a-b;
+let div=a/b;
+let mul=a*b;
+
+console.log("Addition : "+add);
+console.log("Subtraction : "+sub);
+console.log("Multiplication : "+mul);
+console.log("division : "+div);
+
+var x=16;
+var x=8;
+var y=4;
+console.log("Addition is "+(x+y));
+console.log(x+y);
+
+const z=23;	
+console.log(z);
+
+console.log(parseInt(9/3));
+
+console.log("\nprint (1 - 10) numbers");
+//01.print 1 - 10 numbers
+for(let i=1;i<=10;i++)
+{
+	console.log(i);
+}
+console.log("\nprint only the odd numbers");
+//02.print only the odd numbers
+for(let i=0;i<=10;i++)
+{
+	if(i%2==1)
+	{
+		console.log(i);
+	}
+}
+console.log("\nprint the numbers in reverse");
+//03.print the numbers in reverse
+for(let i=10;i>0;i--)
+{
+	console.log(i);
+}
+console.log("\n");
+//04.reverse the numbers in left and right 4321-5-9876
+// let x=4321-5-9876;
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
